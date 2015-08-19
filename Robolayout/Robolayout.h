69692e89e2b5321a158c41f344b832764b6fcfa8
Simon Støvring <simonstoevring@gmail.com>
2015-08-19 10:08:27 +0200

    Initial Commit

---
+++ Robolayout/Robolayout.h
@@ -0,0 +1,19 @@
+//
+//  Robolayout.h
+//  Robolayout
+//
+//  Created by Simon Støvring on 19/08/2015.
+//  Copyright © 2015 Robocat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Robolayout.
+FOUNDATION_EXPORT double RobolayoutVersionNumber;
+
+//! Project version string for Robolayout.
+FOUNDATION_EXPORT const unsigned char RobolayoutVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Robolayout/PublicHeader.h>
+
+
